aa456b8e495f85adbcbdb4f05d50b405b8d36c56
Evan Matherne <evanmatherne55@gmail.com>
2024-04-04 18:46:59 -0500

    post demo

---
+++ Assets/grabKey.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabKey : MonoBehaviour
+{
+
+    public GameObject key;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = key.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, key.transform.position) < 0.4f)
+        {
+            Vector3 dir = Vector3.Normalize(transform.position - key.transform.position);
+
+            rb.AddForce(dir * 10, ForceMode.Force);
+        }
+    }
+}
