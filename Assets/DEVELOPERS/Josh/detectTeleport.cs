dd07c2ce6ff5ebcdd5b1fc958ad23edf9feee11a
joshwilson1212 <joshuawilson2001@gmail.com>
2024-04-05 00:09:34 -0500

    Added Teleport from Jail to ThroneRoom
    it seems my github is detecting changes in some default settings files. i will not push those "changes" and hope that it dose not mess anything else up on you end

---
+++ Assets/DEVELOPERS/Josh/detectTeleport.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectTeleport : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("box"))
+        {
+            print("hit box");
+            transform.position = GameObject.FindWithTag("Thrownroomtp").transform.position;
+            transform.rotation = GameObject.FindWithTag("Thrownroomtp").transform.rotation;
+        }
+        
+        //Rigidbody object = hit.collider.attachedRigidbody;
+    }
+}
