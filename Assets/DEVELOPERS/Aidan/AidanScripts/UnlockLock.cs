6a7c7408dee7cd17738cf6c5614a12e9a9cce56b
Aidan <ams148@email.latech.edu>
2024-04-04 20:13:41 -0500

    stuff

---
+++ Assets/DEVELOPERS/Aidan/AidanScripts/UnlockLock.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UnlockLock : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag("Key")) {
+            Destroy(gameObject);
+        }
+    }
+}
