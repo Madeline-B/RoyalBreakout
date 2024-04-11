0ce3bd585b576b7a270839c8d6069ecfab5254ad
Aidan <ams148@email.latech.edu>
2024-04-11 14:09:29 -0500

    Timer completed, has a cute little frame too

--- Assets/DEVELOPERS/Aidan/AidanScripts/Timer.cs
+++ Assets/DEVELOPERS/Aidan/AidanScripts/Timer.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public TextMeshPro textMeshPro;
+    public float initialTime; // 18000 is 30 minutes in seconds 
+
+    private float currentTime;
+    private bool isCountingDown = true;
+
+    void Start() {
+        currentTime = initialTime;
+        UpdateText();
+        StartCoroutine(Countdown());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator Countdown() {
+        while (isCountingDown) {
+            if (currentTime > 0) {
+                currentTime -= Time.deltaTime;
+                UpdateText();
+            }
+            else {
+                isCountingDown = false;
+                OnTimerEnd(); // triggers the gameover screen
+            }
+
+            yield return null; // wait for the next frame
+        }
+    }
+
+    private void UpdateText() {
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        textMeshPro.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    // put what we want to happen in here
+    private void OnTimerEnd() {
+        Debug.Log("Timer has reached zero.");
     }
 }
